fef981abbd631d4f9a0c4de4541c02fe96f1e960
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-03 23:05:39 +0530

    server is created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+//dot config
+dotenv.config();
+
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Node Server Running In ${process.env.DEV_MODE} ModeOn Port ${process.env.PORT}`
+      .bgBlue.white
+  );
+});
