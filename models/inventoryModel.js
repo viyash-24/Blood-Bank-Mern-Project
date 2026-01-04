41a74fc33564c146063459b84db78b3e48e5025b
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-04 11:07:33 +0530

    inventory model created

---
+++ models/inventoryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema(
+  {
+    inventoryType: {
+      type: String,
+      required: [true, "inventory type require"],
+      enum: ["in", "out"],
+    },
+    bloodGroup: {
+      type: String,
+      required: [true, "blood group is require"],
+      enum: ["O+", "O-", "AB+", "AB-", "A+", "A-", "B+", "B-"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Inventory", inventorySchema);
