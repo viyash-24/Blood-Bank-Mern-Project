68a5005f438f1475601ebb9cdfd5ce18dcde9cf8
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-05 18:36:07 +0530

    donar get controller function is created

---
+++ controllers/adminController.js
@@ -0,0 +1,25 @@
+const userModel = require("../models/userModel");
+
+//GET DONAR LIST
+const getDonarsListController = async (req, res) => {
+  try {
+    const donarData = await userModel
+      .find({ role: "donar" })
+      .sort({ createdAt: -1 });
+
+    return res.status(200).send({
+      success: true,
+      Toatlcount: donarData.length,
+      message: "Donar List Fetched Successfully",
+      donarData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Error In DOnar List API",
+      error,
+    });
+  }
+};
+
