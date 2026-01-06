8b1b5ef1ffc514ff6b47779fc356085a527275df
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-06 18:25:51 +0530

    password is hashed

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,10 @@ const registerController = async (req, res) => {
         message: "User ALready exists",
       });
     }
-   
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+    
 
 module.exports = { registerController, loginController, currentUserController };
