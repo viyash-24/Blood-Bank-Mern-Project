c2674035134c4f31729accd09b9407c2e4b7ab1b
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-07 00:08:11 +0530

    create inventory controller file is created

---
+++ controllers/inventoryController.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const inventoryModel = require("../models/inventoryModel");
+const userModel = require("../models/userModel");
+
+// CREATE INVENTORY
+const createInventoryController = async (req, res) => {
+  try {
+    const { email } = req.body;
+    //validation
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      throw new Error("User Not Found");
+    }
+    // if (inventoryType === "in" && user.role !== "donar") {
+    //   throw new Error("Not a donar account");
+    // }
+    // if (inventoryType === "out" && user.role !== "hospital") {
+    //   throw new Error("Not a hospital");
+    // }
+
+  
