a5fb8a4f420510a39a0fc12c1c77960d6fc7c9db
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-06 18:25:17 +0530

    register controller function is created

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const userModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerController = async (req, res) => {
+  try {
+    const exisitingUser = await userModel.findOne({ email: req.body.email });
+    //validation
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: false,
+        message: "User ALready exists",
+      });
+    }
+   
+
+module.exports = { registerController, loginController, currentUserController };
