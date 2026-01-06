7e9d340711cdfb7bda6afda6e37ec20f1c8529a3
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-06 18:26:23 +0530

    rest data logic is completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,22 @@ const registerController = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     req.body.password = hashedPassword;
-    
+    //rest data
+    const user = new userModel(req.body);
+    await user.save();
+    return res.status(201).send({
+      success: true,
+      message: "User Registerd Successfully",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Register API",
+      error,
+    });
+  }
+};
 
 module.exports = { registerController, loginController, currentUserController };
