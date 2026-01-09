7400123d4ee290eeed2f9fd37f1b2b79b28a2451
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-09 20:57:14 +0530

    return function is added

--- middlewares/authMiddelware.js
+++ middlewares/authMiddelware.js
@@ -4,4 +4,9 @@ module.exports = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
     JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
-      
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Auth Failed",
+        });
+      } 
