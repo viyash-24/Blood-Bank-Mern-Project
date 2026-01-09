c6ce8e8ca59e617afb538c5775cc538fd12ebf1c
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-09 20:56:51 +0530

    JWT is added

--- middlewares/authMiddelware.js
+++ middlewares/authMiddelware.js
@@ -3,4 +3,5 @@ const JWT = require("jsonwebtoken");
 module.exports = async (req, res, next) => {
   try {
     const token = req.headers["authorization"].split(" ")[1];
-   
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      
