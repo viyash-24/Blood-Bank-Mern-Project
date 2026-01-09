4db94dab89996c02e6a2cefe7adbc9f726d6f257
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-09 20:51:12 +0530

    check admin logic is created

---
+++ middlewares/adminMiddleware.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/userModel");
+module.exports = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.body.userId);
+    //check admin
+    if (user?.role !== "admin") {
+      return res.status(401).send({
+        success: false,
+        message: "AUth Fialed",
+      });
+    } else {
+      next();
+    }
+  } 
+};
