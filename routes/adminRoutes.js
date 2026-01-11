9a5b7e041d8343d9185bdebc4221c890919b2019
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-11 18:24:14 +0530

    controllers are imported

---
+++ routes/adminRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const authMiddelware = require("../middlewares/authMiddelware");
+const {
+  getDonarsListController,
+  getHospitalListController,
+  getOrgListController,
+  deleteDonarController,
+} = require("../controllers/adminController");
+const adminMiddleware = require("../middlewares/adminMiddleware");
+
