4117958aae2617ef2065d3ab9b66138917ea11b2
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-18 22:00:39 +0530

    get recent blood records controller is route

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -19,5 +19,12 @@ router.post("/create-inventory", authMiddelware, createInventoryController);
 
 //GET ALL BLOOD RECORDS
 router.get("/get-inventory", authMiddelware, getInventoryController);
+//GET RECENT BLOOD RECORDS
+router.get(
+  "/get-recent-inventory",
+  authMiddelware,
+  getRecentInventoryController
+);
+
 
 module.exports = router;
