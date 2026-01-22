eae0a78551351667f70f65db594fc1e156baad2a
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-22 21:16:33 +0530

    Homepage main function is create

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,3 +6,8 @@ import Layout from "../components/shared/Layout/Layout";
 import Modal from "../components/shared/modal/Modal";
 import API from "../services/API";
 import moment from "moment";
+
+const HomePage = () => {
+  const { loading, error, user } = useSelector((state) => state.auth);
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
