a9ad2cab3ca238e17c8ecd92fd54f9e77062c5e3
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-22 21:12:47 +0530

    handleLogin function is created

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,2 +1,14 @@
 import { userLogin, userRegister } from "../redux/features/auth/authActions";
 import store from "../redux/store";
+
+export const handleLogin = (e, email, password, role) => {
+  e.preventDefault();
+  try {
+    if (!role || !email || !password) {
+      return alert("Please Privde All Feilds");
+    }
+    store.dispatch(userLogin({ email, password, role }));
+  } catch (error) {
+    console.log(error);
+  }
+};
