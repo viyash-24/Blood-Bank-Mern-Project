2710e04bd395345e1eb7740089a178089b610684
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-23 13:46:32 +0530

    final donar recods function created

--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -7,3 +7,12 @@ import { useSelector } from "react-redux";
 const Donation = () => {
   const { user } = useSelector((state) => state.auth);
   const [data, setData] = useState([]);
+  //find donar records
+  const getDonars = async () => {
+    try {
+      const { data } = await API.post("/inventory/get-inventory-hospital", {
+        filters: {
+          inventoryType: "in",
+          donar: user?._id,
+        },
+      });
