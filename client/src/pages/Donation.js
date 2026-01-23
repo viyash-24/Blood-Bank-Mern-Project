b21f952ea543fb937e376142d0312dfe001caf5f
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-23 13:44:39 +0530

    donation function created

--- client/src/pages/Donation.js
+++ client/src/pages/Donation.js
@@ -3,3 +3,7 @@ import React, { useEffect, useState } from "react";
 import Layout from "../components/shared/Layout/Layout";
 import API from "../services/API";
 import { useSelector } from "react-redux";
+
+const Donation = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [data, setData] = useState([]);
