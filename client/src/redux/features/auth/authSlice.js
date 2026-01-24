2eadf9eb4ce2bf5c1cdb6614bb1b5583b852420b
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-24 21:21:37 +0530

    token created

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,2 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { getCurrentUser, userLogin, userRegister } from "./authActions";
+
+const token = localStorage.getItem("token")
+  ? localStorage.getItem("token")
+  : null;
+
+const initialState = {
+  loading: false,
+  user: null,
+  token,
+  error: null,
+};
