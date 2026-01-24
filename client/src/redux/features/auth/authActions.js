948f58825bc49aac4b8e16c7dfc1322cf4ca290b
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-24 21:13:51 +0530

    role eamil password function created

--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -1,3 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import API from "../../../services/API";
 import { toast } from "react-toastify";
+
+export const userLogin = createAsyncThunk(
+  "auth/login",
+  async ({ role, email, password }, { rejectWithValue }) => {
+    try {
+      const { data } = await API.post("/auth/login", { role, email, password });
