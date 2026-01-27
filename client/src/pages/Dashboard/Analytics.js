64b0000c3209a1d492cd9b5f1002a36cf1bc1f27
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-27 19:20:38 +0530

    hooks created

--- client/src/pages/Dashboard/Analytics.js
+++ client/src/pages/Dashboard/Analytics.js
@@ -2,3 +2,17 @@ import React, { useState, useEffect } from "react";
 import Header from "../../components/shared/Layout/Header";
 import API from "./../../services/API";
 import moment from "moment";
+
+const Analytics = () => {
+  const [data, setData] = useState([]);
+  const [inventoryData, setInventoryData] = useState([]);
+  const colors = [
+    "#884A39",
+    "#C38154",
+    "#FFC26F",
+    "#4F709C",
+    "#4942E4",
+    "#0079FF",
+    "#FF0060",
+    "#22A699",
+  ];
