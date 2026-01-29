405d362c98b82b775429a4384b940f0b03cecd96
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-29 20:53:49 +0530

    page ui code is created

--- client/src/pages/Dashboard/Consumer.js
+++ client/src/pages/Dashboard/Consumer.js
@@ -29,4 +29,34 @@ const Consumer = () => {
     getDonars();
   }, []);
 
+  return (
+    <Layout>
+      <div className="container mt-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Blood Group</th>
+              <th scope="col">Inventory TYpe</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Email</th>
+              <th scope="col">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((record) => (
+              <tr key={record._id}>
+                <td>{record.bloodGroup}</td>
+                <td>{record.inventoryType}</td>
+                <td>{record.quantity}</td>
+                <td>{record.email}</td>
+                <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Layout>
+  );
+};
+
 export default Consumer;
