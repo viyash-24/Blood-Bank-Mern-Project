290cbba4ab670dc91f568cf2ac32077b04eb1836
viyash-24 <viyashviyashan2417@gmail.com>
2026-01-31 13:25:41 +0530

    find donar records function created

--- client/src/pages/Dashboard/Donar.js
+++ client/src/pages/Dashboard/Donar.js
@@ -2,3 +2,11 @@ import React, { useEffect, useState } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import API from "../../services/API";
 import moment from "moment";
+
+const Donar = () => {
+  const [data, setData] = useState([]);
+  //find donar records
+  const getDonars = async () => {
+    try {
+      const { data } = await API.get("/inventory/get-donars");
+      //   console.log(data);
