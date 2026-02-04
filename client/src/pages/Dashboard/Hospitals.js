4ade400828a476ebb77292043d6cb1a27be82990
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-04 07:45:36 +0530

    ui code create function

--- client/src/pages/Dashboard/Hospitals.js
+++ client/src/pages/Dashboard/Hospitals.js
@@ -21,3 +21,31 @@ const Hospitals = () => {
   useEffect(() => {
     getDonars();
   }, []);
+
+  return (
+    <Layout>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Address</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((record) => (
+            <tr key={record._id}>
+              <td>{record.hospitalName}</td>
+              <td>{record.email}</td>
+              <td>{record.phone}</td>
+              <td>{record.address}</td>
+              <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
