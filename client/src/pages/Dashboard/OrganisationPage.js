fcc9e77dc5dd806c2e0cf08fff1f1d8d2b47e97e
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-04 07:50:02 +0530

    page frontend created

--- client/src/pages/Dashboard/OrganisationPage.js
+++ client/src/pages/Dashboard/OrganisationPage.js
@@ -31,3 +31,37 @@ const OrganisationPage = () => {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    getOrg();
+  }, [user]);
+
+  return (
+    <Layout>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Address</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((record) => (
+            <tr key={record._id}>
+              <td>{record.organisationName}</td>
+              <td>{record.email}</td>
+              <td>{record.phone}</td>
+              <td>{record.address}</td>
+              <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
+
+
