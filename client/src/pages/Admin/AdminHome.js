91ca6e935c7e32b028e6ffa52b7aa978684022b2
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-05 08:51:39 +0530

    import and create adminHome error function file

---
+++ client/src/pages/Admin/AdminHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import { useSelector } from "react-redux";
+
+const AdminHome = () => {
+  const { user } = useSelector((state) => state.auth);
+  
