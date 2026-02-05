50ecbfcd2ddfe24906eacbe9260f017c88ab5f18
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-05 09:10:04 +0530

    import and donarlist function created

---
+++ client/src/pages/Admin/DonarList.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import Layout from "./../../components/shared/Layout/Layout";
+import moment from "moment";
+import API from "../../services/API";
+
+const DonarList = () => {
+  const [data, setData] = useState([]);
+  
+
+export default DonarList;
