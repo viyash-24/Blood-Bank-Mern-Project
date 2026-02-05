269b5b53e77ffdb71232879c0df1ad310a589571
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-05 08:47:11 +0530

    export file

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -2,3 +2,5 @@ import React from "react";
 import Form from "../../components/shared/Form/Form";
 import { useSelector } from "react-redux";
 import Spinner from "./../../components/shared/Spinner";
+
+export default Login;
