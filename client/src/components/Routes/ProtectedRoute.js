d3c15cf09372c0d45bc0d023a44beff11e37348d
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-07 22:48:49 +0530

    import files

---
+++ client/src/components/Routes/ProtectedRoute.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import API from "../../services/API";
+import { getCurrentUser } from "../../redux/features/auth/authActions";
+import { Navigate } from "react-router-dom";
