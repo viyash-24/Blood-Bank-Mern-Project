927135c0bb53aae210bc88bd309fa461020b4d3d
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-07 22:49:13 +0530

    page logic added

--- client/src/components/Routes/ProtectedRoute.js
+++ client/src/components/Routes/ProtectedRoute.js
@@ -3,3 +3,22 @@ import { useDispatch } from "react-redux";
 import API from "../../services/API";
 import { getCurrentUser } from "../../redux/features/auth/authActions";
 import { Navigate } from "react-router-dom";
+const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch();
+
+  //get user current
+  const getUser = async () => {
+    try {
+      const { data } = await API.get("/auth/current-user");
+      if (data?.success) {
+        dispatch(getCurrentUser(data));
+      }
+    } catch (error) {
+      localStorage.clear();
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  });
