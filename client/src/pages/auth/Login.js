c690b51483080ed358a09c8ce49e812455fcff3d
viyash-24 <viyashviyashan2417@gmail.com>
2026-02-20 10:55:25 +0530

    adding page design

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -19,7 +19,7 @@ const Login = () => {
         <Spinner />
       ) : (
         <div className="row g-0">
-          <div className="col-md-8 form-banner">
+          <div className="col-md-8 form-banner d-none d-md-block">
             <img src="./assets/images/banner1.jpg" alt="loginImage" />
           </div>
           <div className="col-md-4 form-container">
