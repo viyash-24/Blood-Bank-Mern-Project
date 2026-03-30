0e7854dc6cfe744893fadadcf4a2dde3bd97e57b
viyash-24 <viyashviyashan2417@gmail.com>
2026-03-30 20:50:21 +0530

    commands updated

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -9,6 +9,8 @@ const router = express.Router();
 //routes
 
 //GET BLOOD DATA
-router.get("/bloodGroups-data", authMiddelware, bloodGroupDetailsContoller);
+router.get("/bloodGroups-data",
+   authMiddelware,
+    bloodGroupDetailsContoller);
 
 module.exports = router;
