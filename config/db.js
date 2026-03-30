451277a43578894c32a0e2bbcd5a739fe31b32f5
viyash-24 <viyashviyashan2417@gmail.com>
2026-03-30 20:23:14 +0530

    commands updated

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 
+//database connection
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
