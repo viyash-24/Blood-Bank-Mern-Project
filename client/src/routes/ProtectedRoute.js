3c3b8218a7c39868f60e2ec546d8b00a12d3c756
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-01 16:03:18 +0530

    folder structure updated

---
+++ client/src/routes/ProtectedRoute.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentUser } from "../redux/features/auth/authActions";
+import { Navigate } from "react-router-dom";
+const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token && !user) {
+      (async () => {
+        try {
+          const res = await dispatch(getCurrentUser());
+          if (res.meta.requestStatus === "rejected") {
+            localStorage.clear();
+          }
+        } catch (error) {
+          localStorage.clear();
+          console.log(error);
+        }
+      })();
+    }
+  }, [user, dispatch]);
+
+  if (localStorage.getItem("token")) {
+    return children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+};
+
+export default ProtectedRoute;
