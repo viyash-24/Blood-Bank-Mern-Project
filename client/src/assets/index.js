689fbe72421152ab9720c72d3a2cc3724a21038c
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-01 16:05:40 +0530

    folder structure updated

---
+++ client/src/assets/index.js
@@ -0,0 +1,27 @@
+import facebook_icon from './icons/facebook_icon.svg'
+import instagram_icon from './icons/instagram_icon.svg'
+import twitter_icon from './icons/twitter_icon.svg'
+import star_icon from './icons/star_icon.svg'
+import rating_star from './icons/rating_star.svg'
+import email_icon from './icons/email_icon.svg'
+import lock_icon from './icons/lock_icon.svg'
+import cross_icon from './icons/cross_icon.svg'
+import star_group from './images/star_group.png'
+import profile_icon from './images/profile_icon.png'
+import address_icon from './icons/address_icon.svg'
+import phone_icon from './icons/phone_icon.svg'
+
+export const assets = {
+    facebook_icon,
+    instagram_icon,
+    twitter_icon,
+    star_icon,
+    rating_star,
+    email_icon,
+    lock_icon,
+    cross_icon,
+    star_group,
+    profile_icon,
+    address_icon,
+    phone_icon
+}
