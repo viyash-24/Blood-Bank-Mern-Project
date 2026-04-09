4b62506590181f99969ecca8c9d7d595b8965c6e
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-09 12:21:14 +0530

    product route updated

--- client/src/routes/ProtectedRoute.js
+++ client/src/routes/ProtectedRoute.js
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUser } from "../redux/features/auth/authActions";
 import { Navigate } from "react-router-dom";
+
+import Spinner from "../components/Spinner";
 const ProtectedRoute = ({ children }) => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
     if (token && !user) {
       (async () => {
         try {
@@ -21,13 +23,14 @@ const ProtectedRoute = ({ children }) => {
         }
       })();
     }
-  }, []); // Remove user from dependencies to prevent infinite loop
+  }, [dispatch, token, user]);
 
-  if (localStorage.getItem("token")) {
-    return children;
-  } else {
-    return <Navigate to="/login" />;
-  }
+  if (!token) return <Navigate to="/login" replace />;
+
+  // Token exists but user not yet loaded (common after full page reload)
+  if (!user) return <Spinner />;
+
+  return children;
 };
 
 export default ProtectedRoute;
