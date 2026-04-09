76591a9dd4121f458f2e03cf6c93b19a62c194b4
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-09 12:22:33 +0530

    route updated

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const authMiddelware = require("../middlewares/authMiddelware");
+const authMiddleware = require("../middlewares/authMiddleware");
 const {
   bloodGroupDetailsContoller,
 } = require("../controllers/analyticsController");
@@ -10,7 +10,7 @@ const router = express.Router();
 
 //GET BLOOD DATA
 router.get("/bloodGroups-data",
-   authMiddelware,
+  authMiddleware,
     bloodGroupDetailsContoller);
 
 module.exports = router;
