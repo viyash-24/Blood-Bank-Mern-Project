55ca157c23180b83d50f10c0dafdeb1f54a672fe
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-22 20:16:09 +0530

    commands updated

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -11,7 +11,7 @@ const roles = [
   { id: "organisation", label: "Organisation", icon: "🏢" },
 ];
 
-// hooks
+// Hooks
 const Form = ({ formType, submitBtn, formTitle }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
