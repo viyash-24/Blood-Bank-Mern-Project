c1566cdd037e7cd092d784548286d70484e52cae
viyash-24 <viyashviyashan2417@gmail.com>
2026-05-14 22:15:58 +0530

    donar and blood request controller is created

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const {
   registerController,
   loginController,
   currentUserController,
+  changePasswordController,
 } = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
@@ -17,5 +18,8 @@ router.post("/login", loginController);
 
 //GET CURRENT USER || GET
 router.get("/current-user", authMiddleware, currentUserController);
+
+//CHANGE PASSWORD || POST
+router.post("/change-password", authMiddleware, changePasswordController);
 
 module.exports = router;
