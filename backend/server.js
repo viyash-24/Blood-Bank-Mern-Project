99c16e1de13aac6346ce1cf1fdbbdca2b34ee2a7
viyash-24 <viyashviyashan2417@gmail.com>
2026-05-15 20:20:52 +0530

    frontend url is addd

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.FRONTEND_URL ,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
